c8a521a5cc2da4e5b1e4916eb02005fef789853e
csmidt95 <34967998+csmidt95@users.noreply.github.com>
2023-07-27 16:24:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-streamlit.title('Hello World')
+streamlit.title('Hello Worlddd')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣  Omega 3 & Blueberry Oatmeal')
